8766193ce9bc733e5a8f4de4cecb52b6cd21e44d
Jorge Carlos <skay_1994@yahoo.com.br>
2021-08-15 18:28:45 -0300

    Added pagination for single template

--- single.php
+++ single.php
@@ -18,6 +18,28 @@
 				<?= the_content(); ?>
 			</article>
 
+            <h3 class="pb-4 mb-4 fst-italic border-bottom"></h3>
+
+			<nav class="blog-pagination" aria-label="Pagination">
+                <?php
+                $nextPost = '<a class="btn btn-outline-primary disabled" href="#" tabindex="-1" aria-disabled="true">'.__('Next Post').
+                            '<span class="screen-reader-text">'.__( 'Next post') . '</span></a>';
+
+                $prevPost = '<a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">'.__('Previous Post').
+                            '<span class="screen-reader-text">'.__( 'Previous post') . '</span></a>';
+                if($post = get_next_post()) {
+	                $nextPost = '<a class="btn btn-outline-primary" href="'.get_permalink($post).'">'.__('Next Post').
+	                            '<span class="screen-reader-text">'.__('Next Post') .': '. get_the_title().'</span></a>';
+                }
+
+                if($post = get_previous_post()) {
+	                $prevPost = '<a class="btn btn-outline-secondary" href="'.get_permalink( $post ).'">'.__('Previous Post').
+	                            '<span class="screen-reader-text">'.__('Previous Post') .': '. get_the_title().'</span></a>';
+                }
+                echo $nextPost, $prevPost;
+                ?>
+			</nav>
+
 		</div>
 
 		<?php get_sidebar(); ?>
