3550be917be9b61b8d5606caf8b49ce63f4d9ad3
Jorge Carlos <skay_1994@yahoo.com.br>
2021-08-15 18:27:49 -0300

    Created single template

---
+++ single.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+<main class="container">
+	<div class="row g-5">
+		<div class="col-md-8">
+
+			<h3 class="pb-4 mb-4 fst-italic border-bottom"></h3>
+
+			<article class="blog-post">
+				<h2 class="blog-post-title"><?= the_title() ?></h2>
+				<p class="blog-post-meta"><?= the_time('F j, Y').' '.__("by").' '; ?> <a href="<?= get_the_author_posts_link()?>"><?= get_the_author() ?> </a> | <?= get_the_category_list(', ');?></p>
+
+				<?= the_content(); ?>
+			</article>
+
+		</div>
+
+		<?php get_sidebar(); ?>
+	</div>
+
+</main>
+
+<?php get_footer(); ?>
