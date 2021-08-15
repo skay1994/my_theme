edea38ebce07e39890043a80fb8fe95d382f97b5
Jorge Carlos <skay_1994@yahoo.com.br>
2021-08-15 18:33:09 -0300

    Added bootstrap style and script

--- functions.php
+++ functions.php
@@ -1,1 +1,13 @@
 <?php
+
+// Add theme scrips and styles
+function add_theme_scripts_styles()
+{
+	// Load Style
+	wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css', [], '5.1.0');
+	wp_enqueue_style('style', get_stylesheet_uri());
+
+	// Load Scripts
+	wp_enqueue_script('bootstrap_script', "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js", array('jquery'), 2, true);
+}
+add_action('wp_enqueue_scripts', 'add_theme_scripts_styles');
