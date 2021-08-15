5c2ee658462a3a6e931b5fd22d97a6781bb135a2
Jorge Carlos <skay_1994@yahoo.com.br>
2021-08-15 18:31:35 -0300

    Added sidebar show widgets

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,7 @@
 <div class="col-md-4">
 	<div class="position-sticky" style="top: 2rem;">
+		<?php if ( is_active_sidebar( 'home_right_1' )) {
+			dynamic_sidebar( 'home_right_1' );
+        } ?>
 	</div>
 </div>
